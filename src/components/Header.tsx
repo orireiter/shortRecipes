03f217b40531400f93ccea6c84651161862fe5cf
orireiter <ori.reiter@gmail.com>
2022-02-04 01:11:46 +0200

    prettier navbar with functional log out pop up

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 
-import { useAppDispatch } from '../app/hooks';
 import config from '../config.json'
+import { useAppSelector } from '../app/hooks';
+import { useAppDispatch } from '../app/hooks';
 import { openCloseNavbar } from '../slices/generalSettingsSlice';
+import { selectAuth } from '../slices/authSlice';
 
 export default function Header() {
     let pageTitle = config.general.title || 'Short Recipes';
     const dispatch = useAppDispatch();
-    
+    const auth = useAppSelector(selectAuth);
+
     return (
         <div id='navbar'>
+            {(auth.isAuthenticated) ? 
             <div id='navbarToggle'>
                 <span className='material-icons clickable' onClick={() => { dispatch(openCloseNavbar()) }}>
                     menu
                 </span>
-            </div>
+            </div> : null}
             <div id='currentPageTitle'>
                 <h1>{pageTitle}</h1>
             </div>
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,8 +1,43 @@
 import React from 'react';
 
+import Popup from 'reactjs-popup';
+
 import { useAppSelector } from '../app/hooks';
+import { useAppDispatch } from '../app/hooks';
 import { selectGeneralSettings } from '../slices/generalSettingsSlice';
 import { logOut } from '../logic/authLogic';
+
+
+function SignOut() {
+    const dispatch = useAppDispatch();
+
+    return (
+    <Popup modal nested trigger={
+        <div id='navLogout' className='clickable'>
+            <span className='material-icons'>
+                logout
+            </span>
+            <p>Log Out</p>
+        </div>
+        }>
+            { (close: Function) => (
+                <div id='logoutPopup'>
+                    <div>
+                        <h2>Are you sure you want to log out?</h2>
+                    </div>
+                    <div id='logoutPopupButtons'>
+                        <button className='clickable' onClick={() => {close(); dispatch(logOut);}}>
+                            YES
+                        </button>
+                        <button className='clickable' onClick={() => {close();}}>
+                            NO
+                        </button>
+                    </div>
+                </div>
+            )}
+    </Popup>
+    );
+}
 
 
 export default function NavigationBar() {
@@ -13,9 +48,11 @@ export default function NavigationBar() {
             <div>
                 navigation options here
             </div>
-            <div>
-                general operations here
-                <p>sign out</p>
+            <div id='navigationSettings'>
+                < SignOut/>
+                <span id='navSettingsButton' className='material-icons clickable'>
+                    settings
+                </span>
             </div>
         </div>
     );
