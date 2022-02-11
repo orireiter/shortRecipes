006a36fbd77292ab89609442a72bc19ced3e5abc
orireiter <ori.reiter@gmail.com>
2022-02-11 13:55:12 +0200

    add recipe validations

--- src/logic/recipesLogic.tsx
+++ src/logic/recipesLogic.tsx
@@ -1,3 +1,5 @@
+// types
+
 export type ingredient = {
     name: string,
     amountType: string,
@@ -16,4 +18,41 @@ export type recipe = {
     ingredients: Array<ingredient>,
     cookingSteps: Array<cookingStep>,
     tags?: Array<string>
+}
+
+
+// validations
+
+const isValidIngredient = (ingredient: ingredient) => {
+    if (!ingredient.name || !ingredient.amountType || ingredient.amount <= 0) {
+        throw new Error();
+    }
+
+}
+
+const isValidCookingStep = (step: cookingStep) => {
+    if (!step.num || !step.content) {
+        throw new Error();
+    }
+} 
+
+
+export const isValidRecipe = (recipe: recipe) => {
+    if (!recipe.recipeName) {
+        return false;
+    } 
+     
+    try {
+        recipe.ingredients.forEach((ingredient: ingredient) => {
+            isValidIngredient(ingredient);
+        });
+
+        recipe.cookingSteps.forEach((step: cookingStep) => {
+            isValidCookingStep(step);
+        });
+    } catch {
+        return false;
+    }
+
+    return true;
 }
